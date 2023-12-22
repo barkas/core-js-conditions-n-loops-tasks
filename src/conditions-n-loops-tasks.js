7f01b156ed4ce0ca7bc2adf9de8d90f5d6de84ec
barkas <vineda.contact@gmail.com>
2023-12-22 11:04:36 +0200

    feat: add isContainNumber

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -267,8 +267,17 @@ function getIndexOf(str, letter) {
  *  12345, 0    => false
  *  12345, 6    => false
  */
-function isContainNumber(/* num, digit */) {
-  throw new Error('Not implemented');
+function isContainNumber(num, digit) {
+  let res = false;
+  const numStr = String(num);
+  const digitStr = String(digit);
+  for (let i = 0; i < numStr.length; i += 1) {
+    if (numStr[i] === digitStr) {
+      res = true;
+      break;
+    }
+  }
+  return res;
 }
 
 /**
