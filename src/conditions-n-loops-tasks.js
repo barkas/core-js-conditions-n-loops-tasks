6b6398a95f2a2482a20273878d919e37a1fed7e3
barkas <vineda.contact@gmail.com>
2024-01-02 13:01:24 +0200

    feat: add sortByAsc (slow, bubble sort)

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -408,8 +408,19 @@ function rotateMatrix(/* matrix */) {
  *  [2, 9, 5, 9]    => [2, 5, 9, 9]
  *  [-2, 9, 5, -3]  => [-3, -2, 5, 9]
  */
-function sortByAsc(/* arr */) {
-  throw new Error('Not implemented');
+function sortByAsc(arr) {
+  const n = arr.length;
+  const sorted = arr;
+  for (let i = 0; i < n - 1; i += 1) {
+    for (let j = 0; j < n - 1 - i; j += 1) {
+      if (arr[j + 1] < arr[j]) {
+        const t = arr[j + 1];
+        sorted[j + 1] = arr[j];
+        sorted[j] = t;
+      }
+    }
+  }
+  return sorted;
 }
 
 /**
